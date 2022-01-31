2f89014047dab3a604acfd17538f2900fdf0ebce
Abhimanyu Dubey <46393301+dabhimanyu@users.noreply.github.com>
2022-01-31 10:35:07 +0530

    Update bio_watershed_segmentation.m

--- bio_watershed_segmentation.m
+++ bio_watershed_segmentation.m
@@ -1,9 +1,15 @@
 function [  img  ,  BW ]    =   bio_watershed_segmentation( img , min_island_size , ...
     max_island_size , watershed_thresh )
 %
-% This Function carries out the initial segmentation Of the particle
-% phase images and then applies the watershed segmentation to seperate
-% the nearby Connected Components:
+% This Function carries out the initial segmentation Of the TIRF Microscopy
+% images and then applies the watershed segmentation to seperate
+% the nearby Connected Components: 
+%
+% This allows us to segment out the nearby particles and there by eliminate the 
+% error in determination of the centroids of the particles. 
+% 
+% The way this function had been written also makes it a bit robust to the noise 
+% in the images. 
 %
 %   EXAMPLE: MODIFY THIS #####
 %   [ BW_A , A_la ]    =   segmentation_watershed( A_la , watershed_thresh )
@@ -40,19 +46,28 @@ end
 
 
 %% Binarize the Image:
-        
+% We'll first start by binarizing the images. We shall make use of Otsu's Algorithm for this.
         % Binarize the image:
         BW            =       imbinarize(img) ; 
         
 %% Now Apply Watershed Segmentation:
+% Watershed segmentation shall allow us to seperated the two connected segmentations. Now before sending the image 
+% for watershed segmentation we need to ensure that there's minimise the amount of noise in the image. 
+% Watershed segmentation is very sensitive to the amount of noise in the image. So what we'll do is once we have 
+% identified the images, applied a size filter, we'll explicit assign all the reamining pixels zero values. 
+%
+% This will do two important things for us:
+% 1. Reduce the overall noise in the figure. And, 
+% 2. Eliminate the redundant data from the images, which will make the algorithm more robust.
 
 % Preprocessing the binarized Mask:
         SE              =       strel('square' , 2)     ; % Square Structuring element
-        BW              =       imopen(BW , SE )        ; % Opening will remove protrosions in the segmented islands
-        BW              =       imfill(BW ,  'holes')   ; % Filling or closing will fill any hole inside the island
-        BW              =       imclearborder(BW)       ; % Remove Border Pixels
+        BW              =       imopen(BW , SE )        ; % Morphologocal Opening Operation, will remove protrosions in the segmented islands
+        BW              =       imfill(BW ,  'holes')   ; % Filling or Morphological closing will fill any hole inside the island
+        BW              =       imclearborder(BW)       ; % Remove Border Pixels, or else it'll mess with the subsequent steps.
 
 %% Remove all particles which are bigger than the specified limits: 
+% i.e apply a size filter. 
         
         CC              =       bwconncomp(BW , 4) ; 
         CC.islandSize   =       cellfun(@numel , CC.PixelIdxList)' ; 
@@ -74,7 +89,7 @@ end
 % Modify The Original Image Accordingly: 
         img(~BW)        =       0 ;
 
-        d               =       bwdist(~BW) ; 
+        d               =       bwdist(~BW) ;   % Calculating the distance matrix from the 
         d               =       imcomplement(d) ; 
 %       d               =       imhmin(d , watershed_thresh) ; % Images had Very less amount of noise once they were sharpened twice in ImageJ
         particleSep     =       watershed(d) ; % NOTE that particleSep is a labelMatrix
